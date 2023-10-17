6126c6f7b71d42121347824a33d8dde39131d71a
Shazee17 <shahzaman856@gmail.com>
2023-10-17 11:31:11 +0500

    implement dropdown filtering

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,108 +1,106 @@
 import React, { useState } from 'react';
 
 const FilteredTable = ({ data }) => {
-    const [filters, setFilters] = useState({
-      name: '',
-      age: '',
-      city: '',
-      occupation: '',
-    });
-    const [filteredData, setFilteredData] = useState(data);
-  
-    const handleInputChange = (e) => {
-      const { name, value } = e.target;
-      setFilters({ ...filters, [name]: value });
-    };
-  
-    const handleFilterSubmit = (e) => {
-      e.preventDefault();
+  const [filters, setFilters] = useState({
+    name: '',
+    age: '',
+    city: '',
+    occupation: '',
+  });
+  const [filteredData, setFilteredData] = useState(data);
 
-      const filteredResult = data.filter((item) => {
-        return (
-          item.name.includes(filters.name) &&
-          ( item.age === parseInt(filters.age)) &&
-          ( item.city === filters.city) &&
-          ( item.occupation === filters.occupation)
-        );
-      });
-  
-      setFilteredData(filteredResult);
-    };
-
-
-    const resetTable = () => {
-        setFilteredData(data)
-    }
-  
-    return (
-      <div>
-        <h2></h2>
-        <form onSubmit={handleFilterSubmit}>
-          <label>
-            Name:
-            <input
-              type="text"
-              name="name"
-              value={filters.name}
-              onChange={handleInputChange}
-            />
-          </label>
-          <label>
-            Age:
-            <input
-              type="text"
-              name="age"
-              value={filters.age}
-              onChange={handleInputChange}
-            />
-          </label>
-          <label>
-            City:
-            <select name="city" value={filters.city} onChange={handleInputChange}>
-              <option value="">All</option>
-              <option value="City1">New York</option>
-              <option value="City2">San Francisco</option>
-              <option value="City3">Chicago</option>
-              <option value="City4">Los Angeles</option>
-              <option value="City5">Houston</option>
-              <option value="City6">Miami</option>
-              <option value="City7">Seattle</option>
-              <option value="City8">Boston</option>
-              <option value="City9">Denver</option>
-              <option value="City10">Atlanta</option>
-            </select>
-          </label>
-          <label>
-            Occupation:
-            <select
-              name="occupation"
-              value={filters.occupation}
-              onChange={handleInputChange}
-            >
-              <option value="">All</option>
-              <option value="Occupation1">Engineer</option>
-              <option value="Occupation2">Designer</option>
-              <option value="Occupation3">Accountant</option>
-              <option value="Occupation4">Teacher</option>
-              <option value="Occupation5">Doctor</option>
-              <option value="Occupation6">Artist</option>
-              <option value="Occupation7">Software Engineer</option>
-              <option value="Occupation8">Lawyer</option>
-              <option value="Occupation9">Marketing Manager</option>
-              <option value="Occupation10">Entrepreneur</option>
-            </select>
-          </label>
-          <button type="submit">Submit</button>
-        </form>
-
-        <br />
-        <button onClick={resetTable}>Reset</button>
-
-        <Table data={filteredData} />
-      </div>
-    );
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFilters({ ...filters, [name]: value });
   };
 
+  const handleFilterSubmit = (e) => {
+    e.preventDefault();
+    // Filter the data based on the filter values
+    const filteredResult = data.filter((item) => {
+      return (
+        item.name.includes(filters.name) &&
+        (filters.age === '' || item.age === parseInt(filters.age)) &&
+        (filters.city === '' || item.city === filters.city) &&
+        (filters.occupation === '' || item.occupation === filters.occupation)
+      );
+    });
+
+    setFilteredData(filteredResult);
+  };
+
+
+  const resetTable = () => {
+    setFilteredData(data)
+  }
+
+  return (
+    <div>
+      <h2></h2>
+      <form onSubmit={handleFilterSubmit}>
+        <label>
+          Name:
+          <input
+            type="text"
+            name="name"
+            value={filters.name}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          Age:
+          <input
+            type="text"
+            name="age"
+            value={filters.age}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          City:
+          <select name="city" value={filters.city} onChange={handleInputChange}>
+            <option value="">All</option>
+            <option value="City1">New York</option>
+            <option value="City2">San Francisco</option>
+            <option value="City3">Chicago</option>
+            <option value="City4">Los Angeles</option>
+            <option value="City5">Houston</option>
+            <option value="City6">Miami</option>
+            <option value="City7">Seattle</option>
+            <option value="City8">Boston</option>
+            <option value="City9">Denver</option>
+            <option value="City10">Atlanta</option>
+          </select>
+        </label>
+        <label>
+          Occupation:
+          <select
+            name="occupation"
+            value={filters.occupation}
+            onChange={handleInputChange}
+          >
+            <option value="">All</option>
+            <option value="Occupation1">Engineer</option>
+            <option value="Occupation2">Designer</option>
+            <option value="Occupation3">Accountant</option>
+            <option value="Occupation4">Teacher</option>
+            <option value="Occupation5">Doctor</option>
+            <option value="Occupation6">Artist</option>
+            <option value="Occupation7">Software Engineer</option>
+            <option value="Occupation8">Lawyer</option>
+            <option value="Occupation9">Marketing Manager</option>
+            <option value="Occupation10">Entrepreneur</option>
+          </select>
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+
+    <br />
+      <button onClick={resetTable}>Reset</button>
+      <Table data={filteredData} />
+    </div>
+  );
+};
 
 const Table = ({ data }) => {
   if (data && data.length > 0) {
